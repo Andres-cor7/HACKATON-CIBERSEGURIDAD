e928f5612301059104ec078453ae4eb57d7037ee
Andres-cor7 <A01564590@tec.mx>
2026-05-15 12:32:14 -0600

    Fotos

--- src/data/datos.js
+++ src/data/datos.js
@@ -5,19 +5,19 @@ export const POLITICOS = [
     apellido: 'Morales Sánchez',
     puesto: 'Candidato a Diputado Federal',
     distrito: 'Distrito 12 · Jalisco',
-    foto: require('../../assets/politico1.png'), // Tu imagen en assets
+    foto: require('../assets/Dante.jpeg'), // Tu imagen en assets
     colorPrincipal: '#B8860B', // Dorado para este candidato
     propuestas: 'Transparencia y Oportunidades',
   },
   {
     id: '2',
-    nombre: 'Ana María',
-    apellido: 'García Ruiz',
-    puesto: 'Candidata a Senadora',
-    distrito: 'Distrito 5 · Chihuahua',
-    foto: require('../../assets/politico2.png'),
-    colorPrincipal: '#1a5f7a', // Puedes variar los colores por candidato
-    propuestas: 'Educación y Salud Digital',
+    nombre: 'Claudia',
+    apellido: 'Sheinbaum Pardo',
+    puesto: 'Candidata a Presidencia',
+    distrito: 'Mexico',
+    foto: require('../assets/Ruben.jpeg'),
+    colorPrincipal: '#fe047d', // Puedes variar los colores por candidato
+    propuestas: 'Salud y Educacion de calidad',
   },
   // Puedes seguir agregando más bloques así...
 ];
